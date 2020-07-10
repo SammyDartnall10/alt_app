132b13e1bee1dada91dcc7cc994455e842300778
Sammy <samantha.megan10@gmail.com>
2020-07-10 10:59:06 -0400

    first half of POST review route

--- routes/api/review.js
+++ routes/api/review.js
@@ -1,14 +1,41 @@
 const express = require("express");
+const auth = require("../../middleware/auth");
+const Preferences = require("../../models/Preferences");
+const Location = require("../../models/Location");
+const User = require("../../models/User");
 const router = express.Router();
 
 // @route   GET api/posts
 // @desc    Test route
 // @access  Public
-router.get("/", (req, res) => res.send("Reviews route"));
+router.get("/:id", (req, res) => {
+  let profile = Location.findById(req.params.id);
+  return res.json(profile);
+});
 
 // @route   POST api/review/:id
 // @desc    Post a review for a location
 // @access  Private
+router.post("/:id", auth, async (req, res) => {
+  const { review, rating } = req.body;
+
+  //Look up the location being reviewed:
+  const locationReviewed = await Location.findById(req.params.id);
+  const reviewOwner = await User.findById(req.user.id);
+  console.log(locationReviewed);
+
+  //Create the review object
+  const revObj = {};
+  revObj.location = req.params.id;
+  revObj.businessName = locationReviewed.businessName;
+  revObj.user = req.user.id;
+  revObj.reviewedBy = reviewOwner.name;
+  if (review) revObj.review = review;
+  if (rating) revObj.rating = rating;
+
+  console.log(revObj);
+  return res.json(locationReviewed);
+});
 
 // @route   GET api/
 // @desc    Get all reviews for a location
@@ -51,3 +78,67 @@ router.get("/", (req, res) => res.send("Reviews route"));
 // @access  Private
 
 module.exports = router;
+
+//  // the id of the locaton being reviewed - not visible - req.params
+//  location: {
+//   type: Schema.Types.ObjectId,
+//   ref: "location",
+// },
+// // Displayed location name
+// businessName: {
+//   type: String,
+//   required: true,
+// },
+// //The id of the user making the review - not visible req.user
+// user: {
+//   type: Schema.Types.ObjectId,
+//   ref: "users",
+// },
+// // Displayed user name
+// reviewedBy: {
+//   type: String,
+//   required: true,
+// },
+// review: {
+//   type: String,
+//   required: true,
+// },
+// rating: {
+//   type: String,
+//   required: true,
+// },
+// facilities: [PreferencesSchema],
+
+//--------------------------------------------------------
+//   likes: [
+//     {
+//       user: {
+//         type: Schema.Types.ObjectId,
+//         ref: "users",
+//       },
+//     },
+//   ],
+//   comments: [
+//     {
+//       user: {
+//         type: Schema.Types.ObjectId,
+//         ref: "users",
+//       },
+//       text: {
+//         type: String,
+//         required: true,
+//       },
+//       name: {
+//         type: String,
+//       },
+//       date: {
+//         type: Date,
+//         default: Date.now,
+//       },
+//     },
+//   ],
+//   date: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
